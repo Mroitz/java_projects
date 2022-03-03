a9aef60b4f24e158626241edd31f599dc0fccf96
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-03-03 16:40:30 +0100

    for Loop erstellt

---
+++ src/at/moritz/projects/Loops1.java
@@ -0,0 +1,13 @@
+package at.moritz.projects;
+
+public class Loops1 {
+    public static void main(String[] args) {
+
+        int sum;
+
+        for (int i = 1; i <100; i++) {
+            System.out.println();
+
+        }
+    }
+}
