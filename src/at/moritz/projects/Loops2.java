5470d44308c894c506c2e3e648035254f6106d84
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-03-17 16:24:01 +0100

    Loops1 verbesserung + Loops 2 fertig

---
+++ src/at/moritz/projects/Loops2.java
@@ -0,0 +1,15 @@
+package at.moritz.projects;
+
+public class Loops2 {
+    public static void main(String[] args) {
+
+        int a=0;
+
+        for (int i = 2; i <=1000; i+=2) {
+            a += i;
+            System.out.println(a);
+
+
+        }
+    }
+}
