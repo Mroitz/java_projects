8f451d5c44250312e0430dc74c4d1456a31da029
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-03-17 16:20:02 +0100

    Loops1 verbesserung

--- src/at/moritz/projects/Loops1.java
+++ src/at/moritz/projects/Loops1.java
@@ -3,9 +3,11 @@ package at.moritz.projects;
 public class Loops1 {
     public static void main(String[] args) {
 
-        for (int i = 0; i <=100; i++) {
-            System.out.println(i + " ");
 
+        int a = 0;
+        for (int i = 0; i <=100; i++) {
+            a += i;
+            System.out.println(a);
         }
     }
 }
