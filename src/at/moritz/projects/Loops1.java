888382cfc69f293756e18b793268b1cef342f6c0
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-03-17 15:54:10 +0100

    Loops1 done

--- src/at/moritz/projects/Loops1.java
+++ src/at/moritz/projects/Loops1.java
@@ -3,10 +3,8 @@ package at.moritz.projects;
 public class Loops1 {
     public static void main(String[] args) {
 
-        int sum;
-
-        for (int i = 1; i <100; i++) {
-            System.out.println();
+        for (int i = 0; i <=100; i++) {
+            System.out.println(i + " ");
 
         }
     }
