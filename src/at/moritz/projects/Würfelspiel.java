6fd085e231d32de6bf3bb3e7fc6336db1cc3ffaf
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-05-19 16:17:32 +0200

    Würfelspiel Fertig

--- src/at/moritz/projects/Würfelspiel.java
+++ src/at/moritz/projects/Würfelspiel.java
@@ -9,25 +9,57 @@ public class Würfelspiel {
         int sum2 = 0;
         boolean isFinished = false;
 
-        System.out.println("Um zu Würfeln drücken Sie die 1" +
-                "\n" + "Um den Computer würfeln zu lassen drücken Sie die 2" +
-                "\n" + "Drücken sie die 3, wenn Sie das Spiel beenden möchten");
+        System.out.println("\n" + "Throw the dice!" + "\n" +
+                "\n" + "If you want to throw press 1!" +
+                "\n" + "If the Computer should threw press 2!" +
+                "\n" + "If you want to end the game press 3!");
 
         Scanner scanner = new Scanner(System.in);
-
         int selection = scanner.nextInt();
 
-
-        for (int i = 0; i <= 5; i++) {
+        while (!isFinished) {
             if (selection == 1) {
-                Random random = new Random();
-                int randomNumber2 = random.nextInt(1,6);
-                randomNumber2 += sum1;
-                if (i == 5) {
-                    System.out.println(sum1);
+                for (int i = 0; i <= 4; i++) {
+                    Random random = new Random();
+                    int randomNumber2 = random.nextInt(1, 7);
+                    sum1 += randomNumber2;
+                    if (i == 4) {
+                        System.out.println("You threw " + sum1 + "!");
+                    }
+                }
+                selection = scanner.nextInt();
+            }
+            if (selection == 2) {
+                for (int i = 0; i <= 4; i++) {
+                    Random random = new Random();
+                    int randomNumber3 = random.nextInt(1, 7);
+                    sum2 += randomNumber3;
+                    if (i == 4) {
+                        System.out.println("The Computer threw " + sum2 + "!");
+                    }
                 }
             }
-        }
 
+            if (sum2 > sum1) {
+                System.out.println("\n" + "The Computer Won!" + "\n" +
+                        "\n" + "If you want to throw press 1!" +
+                        "\n" + "If the Computer should threw press 2!" +
+                        "\n" + "If you want to end the game press 3!");
+            }
+
+            if (sum1 > sum2) {
+                System.out.println("\n" + "You won!" + "\n" +
+                        "\n" + "If you want to throw press 1!" +
+                        "\n" + "If the Computer should threw press 2!" +
+                        "\n" + "If you want to end the game press 3!");
+            }
+            selection = scanner.nextInt();
+
+            if (selection == 3) {
+                isFinished = true;
+                System.out.println("The game was stopped!");
+            }
+        }
     }
+
 }
