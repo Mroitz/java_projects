b05c342e843e9b58f5091803913a97504a15f0f0
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-09-16 14:30:21 +0200

    Array Aufgabe 2

---
+++ src/at/moritz/projects/Array3.java
@@ -0,0 +1,15 @@
+package at.moritz.projects;
+
+import java.util.Arrays;
+
+public class Array3 {
+    public static void main(String[] args) {
+        int[] number = {8, 3, 2, 22, 8, 1};
+
+
+        Arrays.parallelSort(number, 0,6);
+        for (int i : number) {
+            System.out.println(i);
+        }
+    }
+}
