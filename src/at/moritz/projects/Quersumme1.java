e4378b16c2d9c7d4afcf2b5f5bd268e7bddc556d
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-10-04 10:56:52 +0200

    Quersumme1 fertig

---
+++ src/at/moritz/projects/Quersumme1.java
@@ -0,0 +1,21 @@
+package at.moritz.projects;
+
+import java.util.Random;
+
+public class Quersumme1 {
+    public static void main(String[] args) {
+        for (int number = 0; number < 1000; number++) {
+            String NumberAsString = Integer.toString(number);
+            char[] numbers = NumberAsString.toCharArray();
+            int sum = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                sum += Character.getNumericValue(numbers[i]);
+            }
+
+            if (sum == 15) {
+                System.out.println(numbers);
+                System.out.println(sum);
+            }
+        }
+    }
+}
