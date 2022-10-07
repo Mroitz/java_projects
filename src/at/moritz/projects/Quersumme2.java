3f628e8835373c444c5d6c2ac46f2e5df6685097
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-10-07 14:29:01 +0200

    Quersumme 2 richtig fertig

--- src/at/moritz/projects/Quersumme2.java
+++ src/at/moritz/projects/Quersumme2.java
@@ -5,10 +5,6 @@ public class Quersumme2 {
         for (int number = 0; number < 1000; number++) {
             String NumberAsString = Integer.toString(number);
             char[] numbers = NumberAsString.toCharArray();
-            int sum = 0;
-            for (int i = 0; i < numbers.length; i++) {
-                sum += Character.getNumericValue(numbers[i]);
-            }
 
             if (number % 7 == 0){
                 System.out.println(numbers);
