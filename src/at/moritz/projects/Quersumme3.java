32daeaa8e9266094de7d876fb2de0bd4c4182b89
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-10-11 11:12:15 +0200

    Quersumme 3 fertig

--- src/at/moritz/projects/Quersumme3.java
+++ src/at/moritz/projects/Quersumme3.java
@@ -1,37 +1,41 @@
 package at.moritz.projects;
 
 public class Quersumme3 {
+    public static int[] crosssums = new int[CrossSumCalculation(false) + 1];
     public static void main(String[] args) {
-        int stelle = 0;
-        int anzahl = 1;
+        CrossSumCalculation(true);
+        int highestCrossSum1 = 0;
+        int highestCrossSum2 = 0;
+        int highestEmergence = 0;
 
-        for (int number = 0; number < 1000; number++) {
-            String NumberAsString = Integer.toString(number);
-            char[] numbers = NumberAsString.toCharArray();
-            int sum = 0;
-            for (int i = 0; i < numbers.length; i++) {
-                sum += Character.getNumericValue(numbers[i]);
+        for (int i = 0; i < crosssums.length; i++){
+            if (crosssums[i] == highestEmergence)
+                highestCrossSum2 = i;
+            if (crosssums[i] > highestEmergence) {
+                highestCrossSum1 = i;
+                highestEmergence = crosssums[i];
             }
-            //System.out.println(sum);
-            String sumAsString = Integer.toString(sum);
-            System.out.println(sumAsString);
-        }
-        for (int i = 0; i < sumAsString.lenght; i++) {
-
         }
 
+        System.out.println();
+        if (highestCrossSum1 == highestCrossSum2 || highestCrossSum2 == 0)
+            System.out.println("Highest Cross Sum: " + highestCrossSum1);
+        else
+            System.out.println("Highest Cross Sums: " + highestCrossSum1 + ", " + highestCrossSum2);
+    }
 
-        for (int i = 0; i < sum.length - 1; ++i) {
-
-            if (sum[i] == sum[i + 1]) {
-                ++anzahl;
+    public static int CrossSumCalculation(boolean secondTry){
+        int highestCrossSum = 0;
+        for (int i = 0; i <= 1000; i++) {
+            int crossSum = 0;
+            int tmp = i;
+            while (tmp > 0) {
+                crossSum += tmp % 10;
+                tmp /= 10;
             }
-
+            if(crossSum > highestCrossSum) highestCrossSum = crossSum;
+            if(secondTry) crosssums[crossSum]++;
         }
-        if (arr[i] != arr[i + 1]) {
-            anzahl = 1;
-
-        }
-
+        return highestCrossSum;
     }
 }
