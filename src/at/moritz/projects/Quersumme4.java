787d1ec89c1d9eddbe3b64a691c03c4d915af1e0
Mroitz <moritz.brandner@student.htldornbirn.at>
2022-10-14 14:14:45 +0200

    Quersumme 4 fertig

---
+++ src/at/moritz/projects/Quersumme4.java
@@ -0,0 +1,22 @@
+package at.moritz.projects;
+
+import java.util.Scanner;
+
+public class Quersumme4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        while (number > 10){
+            String NumberAsString = Integer.toString(number);
+            char[] number2 = NumberAsString.toCharArray();
+            int sum = 0;
+            for (int i = 0; i < number2.length; i++) {
+                sum += Character.getNumericValue(number2[i]);
+            }
+            number = sum;
+            System.out.println(sum);
+        }
+    }
+}
+
