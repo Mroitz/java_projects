cb52502dd9dfd28bcb1c3cff1f768bd454c8c479
Mroitz <moritz.brandner@student.htldornbirn.at>
2023-01-24 11:24:48 +0100

    05 Refactoring Fertig

--- src/at/moritz/projects/cars/cars.java
+++ src/at/moritz/projects/cars/cars.java
@@ -2,38 +2,74 @@ package at.moritz.projects.cars;
 
 public class cars {
 
-    public int fuelconsumption;
-    public String serialnumber;
+    private int fuelconsumption;
+    private String serialnumber;
     private String color;
-    public String brand;
-    public int fuelcapacity;
-    public int aktivfuel;
-    public int range;
+    private String brand;
+    private int fuelcapacity;
+    private int aktivfuel;
+    private int range;
 
-    public void drive(){
-        this.aktivfuel = this.fuelcapacity - this.fuelconsumption;
-        System.out.println("fuelamount is " + this.aktivfuel +" liters");
+    public void setColor(String color) {
+        this.color = color;
     }
 
-    public void Break(){
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setAktivfuel(int aktivfuel) {
+        this.aktivfuel = aktivfuel;
+    }
+
+    public void setFuelcapacity(int fuelcapacity) {
+        this.fuelcapacity = fuelcapacity;
+    }
+
+    public void setFuelconsumption(int fuelconsumption) {
+        this.fuelconsumption = fuelconsumption;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public void setSerialnumber(String serialnumber) {
+        this.serialnumber = serialnumber;
+    }
+
+    public cars(int fconsumtion, String snumber, String color, String brand, int fcapacity) {
+        this.fuelconsumption = fconsumtion;
+        this.fuelcapacity = fcapacity;
+        this.color = color;
+        this.brand = brand;
+        this.serialnumber = snumber;
+    }
+
+    public void drive() {
+        this.aktivfuel = this.fuelcapacity - this.fuelconsumption;
+        System.out.println("fuelamount is " + this.aktivfuel + " liters");
+    }
+
+    public void Break() {
         System.out.println("Ich bremse");
     }
 
-    public void turboBoost(){
-        if(this.aktivfuel > (0.1 * this.fuelcapacity)) {
+    public void turboBoost() {
+        if (this.aktivfuel > (0.1 * this.fuelcapacity)) {
             System.out.println("SuperBoostMode");
-        }
-        else{
+        } else {
             System.out.println("Not enough fuel to go to SuperBoostMode");
         }
     }
-    public void honk(int amountOfRepetitions){
+
+    public void honk(int amountOfRepetitions) {
         for (int i = 0; i < amountOfRepetitions; i++) {
             System.out.println("TUUUT!");
         }
     }
 
-    public void getRemainingRange(){
+    public void getRemainingRange() {
         range = this.aktivfuel / this.fuelconsumption;
         System.out.println(range + "km");
     }
--- src/at/moritz/projects/cars/main.java
+++ src/at/moritz/projects/cars/main.java
@@ -2,25 +2,16 @@ package at.moritz.projects.cars;
 
 public class main {
     public static void main(String[] args) {
-        cars c1 = new cars();
-        c1.fuelconsumption = 7;
-        c1.brand = "audi";
-        c1.serialnumber = "A1234";
-        c1.fuelcapacity = 70;
+        cars c1 = new cars(7, "123A", "white", "Audi", 70);
+        c1.setFuelconsumption(7);
+        c1.setBrand("Audi");
+        c1.setSerialnumber("A1234");
+        c1.setFuelcapacity(70);
 
         c1.drive();
-
-        cars c2 = new cars();
-        c2.fuelconsumption = 11;
-        c2.brand = "Ford";
-        c2.serialnumber = "A1234";
-        c2.fuelcapacity = 100;
-
         c1.Break();
         c1.turboBoost();
-
         c1.honk(1);
-
-        c2.getRemainingRange();
+        c1.getRemainingRange();
     }
 }
